778ec5e18d8ec825757706c2b0951312c34f6b90
Mikita Pilinka <mikita.pilinka@northern.tech>
2024-12-07 23:32:28 +0200

    test healtcheck works

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  throw new Error('verify healthCheck work\'s')
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
